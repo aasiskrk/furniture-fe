888dfb10c07c3c28ffc3bbe95ae43fe2ef651592
aasiskrk <aasis.krk1@gmail.com>
2025-08-22 14:09:34 +0545

    k

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../server/certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../server/certs/cert.pem')),
-    },
+    https:
+      process.env.NODE_ENV === 'development'
+        ? {
+            key: fs.readFileSync(path.resolve(__dirname, '../server/certs/key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, '../server/certs/cert.pem')),
+          }
+        : false,
     proxy: {
       '/api': {
         target: 'https://localhost:5000',
